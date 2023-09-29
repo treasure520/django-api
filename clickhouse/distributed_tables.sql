4d766b7674fac32f088aaa9866dd64867a146894
treasure520 <treasure.you@gmail.com>
2023-09-29 17:42:50 +0800

    Delete clickhouse directory

--- clickhouse/distributed_tables.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE tutorial.hits_v1_all ON CLUSTER '{cluster}' AS tutorial.hits_v1_local
-ENGINE Distributed('{cluster}', 'tutorial', 'hits_v1_local', rand());
-
-CREATE TABLE tutorial.visits_v1_all ON CLUSTER '{cluster}' AS tutorial.visits_v1_local
-ENGINE = Distributed('{cluster}', 'tutorial', 'visits_v1_local', rand());
